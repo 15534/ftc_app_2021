43c0ca78f85c485e2d0a01c1f0cf27d1a4ad147b
nch0w <neil.chowdhury@outlook.com>
2020-10-27 10:50:36 -0400

    Remove empty/useless programs, reduce power in ShooterTest
    Signed-off-by: nch0w <neil.chowdhury@outlook.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainTest.java
@@ -6,11 +6,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Shooter Test")
-public class ShooterTest extends LinearOpMode {
+@TeleOp(name="Drivetrain Test")
+public class DrivetrainTest extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor rearLeft = null;
+    private DcMotor rearRight = null;
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
 
     @Override
     public void runOpMode() {
@@ -20,13 +24,15 @@ public class ShooterTest extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        DcMotor leftDrive = hardwareMap.get(DcMotor.class, "left");
-        DcMotor rightDrive = hardwareMap.get(DcMotor.class, "right");
+        rearLeft  = hardwareMap.get(DcMotor.class, "rear_left");
+        rearRight = hardwareMap.get(DcMotor.class, "rear_right");
+        frontLeft  = hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -35,8 +41,10 @@ public class ShooterTest extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             // Send calculated power to wheels
-            leftDrive.setPower(0.64489);
-            rightDrive.setPower(0.64489);
+            rearLeft.setPower(0.2);
+            rearRight.setPower(0.2);
+            frontLeft.setPower(0.2);
+            frontRight.setPower(0.2);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Running");
