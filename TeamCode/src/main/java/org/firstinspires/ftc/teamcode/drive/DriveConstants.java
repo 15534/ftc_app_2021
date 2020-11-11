fbbe5a253e5c83ae36ee114089e4c7dbe19d719b
nch0w <neil.chowdhury@outlook.com>
2020-11-11 18:37:23 -0500

    update kV, kA, and PID coefficients - localization still needs tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.9685; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 30; // in
+    public static double TRACK_WIDTH = 15.7; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -56,8 +56,8 @@ public class DriveConstants {
      * empirically tuned.
      */
     //public static double kV = (1.0 / rpmToVelocity(MAX_RPM))+0.002;
-    public static double kV = 0.0235;
-    public static double kA = 0;
+    public static double kV = 0.019;
+    public static double kA = 0.002;
     public static double kStatic = 0;
 
     /*
