fad125727ce0b495a030e47e7e171dbd0652b63a
nch0w <neil.chowdhury@outlook.com>
2020-11-22 15:33:39 -0500

    Add Wobble Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -7,6 +7,7 @@ import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -14,7 +15,10 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import static org.firstinspires.ftc.teamcode.Wobble.GRIPPER_RELEASE;
 
 @TeleOp(name="Tele-op")
 @Config
@@ -36,15 +40,21 @@ public class MainTeleOp extends LinearOpMode {
         // 0.75 - pushed position
 
         GamepadEx gp1 = new GamepadEx(gamepad1);
+        ButtonReader wobbleButtonReader = new ButtonReader(gp1, GamepadKeys.Button.A);
         ButtonReader pushButtonReader = new ButtonReader(gp1, GamepadKeys.Button.X);
         ButtonReader toggleIndexer = new ButtonReader(gp1, GamepadKeys.Button.Y);
         double lastPushTime = -1;
         boolean indexerActive = false;
 
+//        ModernRoboticsI2cRangeSensor rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range_1");
+        Wobble wobble = new Wobble(hardwareMap);
+        wobble.armUp();
+
         waitForStart();
         while (!isStopRequested()) {
             pushButtonReader.readValue();
             toggleIndexer.readValue();
+            wobbleButtonReader.readValue();
             if (Math.abs(gamepad1.left_stick_x) > 0.05 || (Math.abs(gamepad1.left_stick_y) > 0.05)
                     || (Math.abs(gamepad1.right_stick_x) > 0.05)) {
                 if (Math.abs(gamepad1.right_stick_x) < 0.05) {
@@ -83,6 +93,19 @@ public class MainTeleOp extends LinearOpMode {
             } else {
                 indexer.setPower(0);
             }
+
+            if (wobbleButtonReader.wasJustPressed()) {
+                if (wobble.armUp) {
+                    wobble.armDown();
+                } else {
+                    wobble.armUp();
+                }
+            }
+            wobble.setGripper(GRIPPER_RELEASE);
+
+            telemetry.addData("wobble gripper", "%.2f", wobble.getGripper());
+//            telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
+//            telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
             telemetry.update();
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wobble.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+public class Wobble {
+    public Servo wobbleArm, wobbleGripper;
+    public PwmControl wobbleGripperPwm;
+
+    public static double GRIPPER_RELEASE = 0.5;
+    public static double ARM_UP = 0.15;
+    public static double ARM_DOWN = 0.92;
+
+    public boolean armUp = true;
+
+    public Wobble (HardwareMap hardwareMap) {
+        wobbleArm = hardwareMap.get(Servo.class, "wobble_arm");
+        wobbleGripper = hardwareMap.get(Servo.class, "wobble_gripper");
+        wobbleGripperPwm = (PwmControl) wobbleGripper;
+
+        PwmControl.PwmRange range = new PwmControl.PwmRange(800, 2200);
+        wobbleGripperPwm.setPwmRange(range);
+    }
+
+    public void setArm(double position) {
+        wobbleArm.setPosition(position);
+    }
+
+    public void armUp () {
+        wobbleArm.setPosition(ARM_UP);
+        armUp = true;
+    }
+
+    public void armDown() {
+        wobbleArm.setPosition(ARM_DOWN);
+        armUp = false;
+    }
+
+    public void setGripper(double position) {
+        wobbleGripper.setPosition(position);
+    }
+    public double getArm() {
+        return wobbleArm.getPosition();
+    }
+
+    public double getGripper() {
+        return wobbleGripper.getPosition();
+    }
+}
