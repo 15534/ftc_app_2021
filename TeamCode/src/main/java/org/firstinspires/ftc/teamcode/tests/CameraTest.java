8e610062e7ae061268d286dbed09fbbf7c65b29b
nch0w <neil.chowdhury@outlook.com>
2021-03-04 18:43:03 -0500

    Add OpenCV starter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/CameraTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Wobble;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Config
+@TeleOp(name="Camera Test")
+public class CameraTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                // Usually this is where you'll want to start streaming from the camera (see section 4)
+//                camera.setViewportRenderer(OpenCvCamera.ViewportRenderer.GPU_ACCELERATED);
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+        });
+
+        waitForStart();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ServoTest.java
@@ -1,18 +1,38 @@
 package org.firstinspires.ftc.teamcode.tests;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Wobble;
 
+@Config
 @TeleOp(name="Wobble Gripper Test")
 public class ServoTest extends LinearOpMode {
+    public static double pos = 0;
+//    public Servo gripperServo;
+//    public PwmControl gripperServoPWM;
+
     @Override
     public void runOpMode() throws InterruptedException {
+//
+//        gripperServo = hardwareMap.servo.get("wobble_gripper");
+//        gripperServo.setDirection(Servo.Direction.FORWARD);
+//        gripperServo.scaleRange(0, 1);
+//        gripperServoPWM = (PwmControl) gripperServo;
+//        PwmControl.PwmRange range = new PwmControl.PwmRange(800, 2200);
+//        gripperServoPWM.setPwmRange(range);
+//        gripperServoPWM.setPwmEnable();
+
+        telemetry.addData("hello", "");
+        telemetry.update();
         Wobble wobble = new Wobble(hardwareMap);
         waitForStart();
+//        gripperServo.setPosition(0.5);
         while (opModeIsActive()) {
-            double pos = (1.0 + gamepad1.right_stick_y)/2.0;
+//            double pos = (1.0 + gamepad1.right_stick_y)/2.0;
             wobble.setGripper(pos);
             telemetry.addData("position", pos);
             telemetry.update();
